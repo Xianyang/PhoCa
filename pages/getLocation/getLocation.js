fb4775d7bbd726bb188b1f19e69e0a7271b72ee0
xianyang <xianyang@MBP-xy.local>
2017-07-25 13:47:18 +0800

    add location map

--- pages/getLocation/getLocation.js
+++ pages/getLocation/getLocation.js
@@ -5,15 +5,20 @@ var formatLocation = util.formatLocation
 Page({
   data: {
     hasLocation: false,
+    loading: false,
   },
   fetchLocation: function () {
     console.log("start to get location")
     var that = this
+    that.setData({
+      loading: true,
+    })
     wx.getLocation({
       success: function (res) {
         console.log(res)
         that.setData({
           hasLocation: true,
+          loading: false,
           location: formatLocation(res.longitude, res.latitude)
         })
       }
--- pages/locationMap/locationMap.js
+++ pages/locationMap/locationMap.js
@@ -5,10 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    loading: false,
   },
 
   openLocation: function() {
-    console.log("get location")
+    console.log("getting current location")
+    var that = this
+    that.setData({
+      loading: true,
+    })
+    wx.getLocation({
+      success: function (res) {
+        that.setData({
+          loading: false
+        })
+        console.log(res)
+        wx.openLocation({
+          longitude: Number(res.longitude),
+          latitude: Number(res.latitude),
+          // name: "name?",
+          // address: "address?"
+        })
+      }
+    })
   },
 })
